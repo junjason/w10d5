4bc725a03c0bcd9e0bf4342ab61f9e487ec2db7a
Jason Jun <junjason@umich.edu>
2023-10-31 17:17:30 -0400

    working on phase 2

--- practice-for-week-14-react-router-art-museum-long-practice-main/art-museum/src/App.js
+++ practice-for-week-14-react-router-art-museum-long-practice-main/art-museum/src/App.js
@@ -1,12 +1,22 @@
+import { Route } from "react-router-dom/cjs/react-router-dom";
 import { GalleryNavigation } from "./components/GalleryNavigation";
+import { GalleryView } from "./components/GalleryView";
 import harvardArt from "./data/harvardArt";
 
 
 function App() {
   return (
-    <GalleryNavigation galleries={harvardArt.records}> 
-    </GalleryNavigation>
-  );
+    <>
+      <GalleryNavigation galleries={harvardArt.records}> 
+      </GalleryNavigation>
+      <Route>
+        <GalleryView>
+        </GalleryView>
+      </Route>
+    </>
+    
+  )
+      
 }
 
 export default App;
---
+++ practice-for-week-14-react-router-art-museum-long-practice-main/art-museum/src/components/GalleryView/index.jsx
@@ -0,0 +1,7 @@
+
+
+export const GalleryView = () => {
+    return (
+        <h1>Hi</h1>
+    )
+}
